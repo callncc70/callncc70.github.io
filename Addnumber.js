f7d205c880d69f4db00b787d759e911b6a811d8c
callncc70 <callncc70@gmail.com>
2020-10-08 11:10:16 -0600

    final

--- Addnumber.js
+++
@@ -1,9 +0,0 @@
-function addNumbers() {
-	//INPUT
-	 let x = parseFloat(document.getElementById('number1').value);
-	 let y = parseFloat(document.getElementById('number2').value);
-	//PROCESSING
-	 let sum = x + y;
-	//OUTPUT
-	 document.getElementById("output").innerHTML = "The sum of " + x + " and " + y + " equals " + sum + "!";
-}
